df03aa66bd55efa8e3ee9a41a0478a6fcf66c8ec
plankl <sebastian.plankl@gmail.com>
2025-08-12 18:24:35 +0200

    new version

---
+++ service-worker.js
@@ -0,0 +1,58 @@
+
+const CACHE_NAME = 'hydrantenkarte-ar-v8.0';
+const urlsToCache = [
+  'index_hybrid.html',
+  'manifest.json',
+  'service-worker.js',
+  'icon.png',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://cdn.jsdelivr.net/gh/aframevr/aframe@1c2407b26c61958baa93967b5412487cd94b290b/dist/aframe-master.min.js',
+  'https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache geöffnet');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(error => {
+        console.error('Cache-Fehler:', error);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+      .catch(error => {
+        console.error('Fetch-Fehler:', error);
+        if (event.request.destination === 'document') {
+          return caches.match('index_hybrid.html');
+        }
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Lösche alten Cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
